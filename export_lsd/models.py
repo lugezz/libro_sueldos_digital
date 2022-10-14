7d26a3e9b54983a157d57409b34bfe333ff8b2e3
Eugenio <lugezz@gmail.com>
2022-10-14 14:47:47 -0300

    Some formatting

--- export_lsd/models.py
+++ export_lsd/models.py
@@ -10,6 +10,12 @@ DATA_TYPE = [
     ('NU', 'Numérico'),
 ]
 
+FORMAS_PAGO = [
+    (1, 'Efectivo'),
+    (2, 'Cheque'),
+    (3, 'Acreditación'),
+]
+
 
 class TipoRegistro(models.Model):
     name = models.CharField(max_length=120)
@@ -96,3 +102,34 @@ class Registro(models.Model):
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     employees = models.PositiveSmallIntegerField()
     created = models.DateTimeField(auto_now_add=True)
+
+
+class BasicExportConfig(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120, verbose_name='Nombre')
+    dia_base: models.PositiveSmallIntegerField()
+    'forma_pago': 1,
+    'ccn_sueldo': 'SUELDO',
+    'ccn_no_rem': 'ASNORE',
+    'ccn_no_osysind': 'ACUENR',
+    'ccn_no_sind': 'PRUEBA',
+    'ccn_sijp': 'JUBILA',
+    'ccn_inssjp': 'INSSJP',
+    'ccn_os': 'OBRSOC',
+    'ccn_sindicato': 'SINDIC',
+    'porc_sindicato': 4.5,
+    'tipo_nr': 2,  # 0: Sólo NR, 1: Base Sindicato, 2: Base Sindicato y Obra Social
+    'area': 'Administración',
+    'cuit_empleador_eventuales': 30692273725,
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        user = get_current_user()
+        if user and not user.pk:
+            user = None
+        if not self.pk:
+            self.user = user
+        self.user = user
+        return super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self) -> str:
+        return f'{self.user} - {self.name}'
